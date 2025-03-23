bb70b99e28458d64bbfcbc81be6bfd4be4501493
T Rama Krishna <117737866+tramakrishna3012@users.noreply.github.com>
2025-03-23 14:23:21 +0530

    change

---
+++ backend/cors/corsConfig.js
@@ -0,0 +1,6 @@
+var corsOptions = {
+
+  origin: ["swasthya-sampark.vercel.app","https://www.swasthya-sampark.me"] // development
+};
+module.exports = corsOptions;
+
---
+++ swasthyaSampark/src/components/consultation/services/api.ts
@@ -0,0 +1,9 @@
+const BACKEND_URL = "https://swasthya-sampark.onrender.com";
+const ML_URL = "http://ec2-13-60-76-84.eu-north-1.compute.amazonaws.com:8080/";//AWS
+// const BACKEND_URL = "https://swasthya-sampark.onrender.com";
+// const BACKEND_URL = "https://connectihealth-backend.vercel.app"; // production1 state-less
+// const BACKEND_URL = "https://connect-health.onrender.com"; // production2 state-full
+export {
+    BACKEND_URL,
+    ML_URL,
+};
