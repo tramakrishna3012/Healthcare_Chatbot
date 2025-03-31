0358bd7580d60b606d21044bb40709ce74f6c567
T Rama Krishna <117737866+tramakrishna3012@users.noreply.github.com>
2025-03-31 09:33:10 +0530

    ch

--- backend/database/patientRequestDoctor.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require("mongoose");
-let Schema = new mongoose.Schema({
-  email: String,
-  reqeustDoctors: [String],
-});
-let PatientRequestDoctor = mongoose.model("PatientRequestDoctor", Schema);
-module.exports = PatientRequestDoctor;
